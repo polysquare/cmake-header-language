f0f740f8acce05a00b2987cc835bc016aa7a53a3
Sam Spilsbury <smspillaz@gmail.com>
2016-03-07 11:05:32 +0800

    conan: Add support for deploying to conan.io

---
+++ conanfile.py
@@ -0,0 +1,30 @@
+from conans import ConanFile
+from conans.tools import download, unzip
+import os
+
+VERSION = "0.0.1"
+
+
+class CMakeHeaderLanguageConan(ConanFile):
+    name = "cmake-header-language"
+    version = os.environ.get("CONAN_VERSION_OVERRIDE", VERSION)
+    generators = "cmake"
+    requires = ("cmake-include-guard/master@smspillaz/cmake-include-guard",
+                "cmake-unit/master@smspillaz/cmake-unit")
+    url = "http://github.com/polysquare/cmake-header-language"
+    license = "MIT"
+
+    def source(self):
+        zip_name = "cmake-header-language.zip"
+        download("https://github.com/polysquare/"
+                 "cmake-header-language/archive/{version}.zip"
+                 "".format(version="v" + VERSION),
+                 zip_name)
+        unzip(zip_name)
+        os.unlink(zip_name)
+
+    def package(self):
+        self.copy(pattern="*.cmake",
+                  dst="cmake/cmake-header-language",
+                  src="cmake-header-language-" + VERSION,
+                  keep_path=True)
